2e0b0072126359d96f6094879d68f85c3b27280d
David Lenwell <dlenwell@gmail.com>
2021-10-08 14:22:01 -0700

    added iterative queue invert function and added better doc strings

--- code-samples/python/inverting-trees/invert-tree.py
+++ code-samples/python/inverting-trees/invert-tree.py
@@ -1,20 +1,68 @@
 """
+Tree inversion:
 
+Inverting a binary tree is a common programming task in tech screening
+interviews.
 
-Tree inversion
+Example output:
+generating tree with 100 nodes with random values between 1 and 999
 
+     ____346______________________
+    /                             \
+  _181_          ________________973
+ /     \        /
+138   335     _576_
+             /     \
+            502   599__________
+                               \
+                          ____843
+                         /
+                       _764_
+                      /     \
+                     759   818
 
+Inverting tree with recursion:
 
+  ______________________346____
+ /                             \
+973________________          _181_
+                   \        /     \
+                 _576_     335   138
+                /     \
+     __________599   502
+    /
+   843____
+          \
+        _764_
+       /     \
+      818   759
 
+Inverting tree with iterative style:
+
+     ____346______________________
+    /                             \
+  _181_          ________________973
+ /     \        /
+138   335     _576_
+             /     \
+            502   599__________
+                               \
+                          ____843
+                         /
+                       _764_
+                      /     \
+                     759   818
 
 """
 from tree import Node, Tree
-
+from collections import deque
 
 class InvertTree(Tree):
     """
     InvertTree Tree class extends the base Tree class and just adds the invert
     functions.
+
+    This class includes a recursive and an iterative version of the function.
 
     """
 
@@ -33,12 +81,29 @@ class InvertTree(Tree):
         return(node)
 
 
-    def invert(self):
+    def recursive_invert(self):
         """
         invert tree
         """
         self.invert_aux(self.root)
 
+
+    def invert_iterative(self):
+        """
+        iterative function to invert the binary tree using a queue
+        """
+        queue = deque()
+        queue.append(self.root)
+
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left is not None:
+                queue.append(current.left)
+
+            if current.right is not None:
+                queue.append(current.right)
 
 
 def main():
@@ -53,7 +118,7 @@ def main():
     print('generating tree with 100 nodes with random values between 1 and 999')
 
     # add 100 random values to the tree
-    for i in range(10):
+    for i in range(12):
         value = random.randint(1,999)
         unsorted_node_values.append(value)
         tree.add(value)
@@ -61,11 +126,17 @@ def main():
     print()
     tree.print()
     print()
-    print('Inverting tree:')
+    print('Inverting tree with recursion:')
 
-    tree.invert()
+    tree.recursive_invert()
     print()
     tree.print()
+    print()
+    print('Inverting tree with iterative style:')
+    tree.invert_iterative()
+    print()
+    tree.print()
+
     print()
     print()
 
