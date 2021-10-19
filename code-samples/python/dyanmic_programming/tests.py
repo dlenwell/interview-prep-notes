a97fe9b72bad8d360c6fd1500bf9de2acb51651f
David Lenwell <dlenwell@gmail.com>
2021-10-19 14:52:58 -0700

    added has sum unit test

--- code-samples/python/dyanmic_programming/tests.py
+++ code-samples/python/dyanmic_programming/tests.py
@@ -9,6 +9,64 @@ from memoization_best_sum import best_sum_memo
 from memoization_can_construct import can_construct_memo
 from memoization_can_sum import can_sum_memo
 from memoization_fib import fib
+from memoization_grid_traveler import grid_traveler_memo
+from memoization_has_sum import has_sum_memo
+
+
+class TestHasSum(unittest.TestCase):
+    """
+    tests if results match expected results
+    """
+    scenerios = [
+        {
+            'target': 300,
+            'numbers': [7, 14],
+            'assert': None
+        },
+        {
+            'target': 100,
+            'numbers': [10, 25],
+            'assert': [25, 25, 25, 25]
+        }
+    ]
+
+    def test_has_sum(self):
+        """
+        test for can construct memoized
+        """
+        for scenerio in self.scenerios:
+            self.assertEqual(
+                has_sum_memo(scenerio['target'], scenerio['numbers']),
+                scenerio['assert']
+            )
+
+
+class TestGridTraveler(unittest.TestCase):
+    """
+    tests if results match expected results
+    """
+    scenerios = [
+        {
+            'height': 10,
+            'width': 10,
+            'assert': 48620
+        },
+        {
+            'height': 12,
+            'width': 12,
+            'assert': 705432
+        },
+    ]
+
+    def test_grid_traveler_memo(self):
+        """
+        test for fib
+        """
+        for scenerio in self.scenerios:
+            self.assertEqual(
+                grid_traveler_memo(scenerio['height'], scenerio['width']),
+                scenerio['assert']
+            )
 
 
 class TestFib(unittest.TestCase):
