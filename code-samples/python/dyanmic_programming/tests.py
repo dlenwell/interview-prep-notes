f14b1afcea075ad8fedc064801903f3bb8e6be6e
David Lenwell <dlenwell@gmail.com>
2021-10-19 13:57:22 -0700

    added test can sum test

--- code-samples/python/dyanmic_programming/tests.py
+++ code-samples/python/dyanmic_programming/tests.py
@@ -5,7 +5,76 @@ folder.
 
 """
 import unittest
-from memoization_best_sum import best_sum_rec, best_sum_memo
+from memoization_best_sum import best_sum_memo
+from memoization_can_construct import can_construct_memo
+from memoization_can_sum import can_sum_memo
+
+
+class TestCanSum(unittest.TestCase):
+    """
+    tests if results match expected results
+    """
+    scenerios = [
+        {
+            'target': 100,
+            'numbers': [7, 14],
+            'assert': False
+        },
+        {
+            'target': 100,
+            'numbers': [25, 7, 14],
+            'assert': True
+        }
+    ]
+
+    def test_can_sum(self):
+        """
+        test for can construct memoized
+        """
+        for scenerio in self.scenerios:
+            self.assertEqual(
+                can_sum_memo(scenerio['target'], scenerio['numbers']),
+                scenerio['assert']
+            )
+
+
+class TestCanConstruct(unittest.TestCase):
+    """
+    tests if results match expected results
+    """
+    scenerios = [
+        {
+            'target': "abcdef",
+            'words': ['ab', 'abc', 'cd', 'def', 'abcd'],
+            'assert': True
+        },
+        {
+            'target': "skateboard",
+            'words': ['bo', 'rd', 'ate', 't', 'ska', "sk", "boar"],
+            'assert': False
+        },
+        {
+            'target': "enterapotentpot",
+            'words': ["a", "p", "ent", "enter", "ot", "o", "t"],
+            'assert': True
+        },
+        {
+            'target': "running can_construct_rec with target.",
+            'words': ["run", "n", "in", "g", "con", " ", "_", "tar",
+                      "ge", "t", ".", "c", "a", "struct_rec", "with"],
+            'assert': True
+        }
+    ]
+
+    def test_can_construct_memo(self):
+        """
+        test for can construct memoized
+        """
+        for scenerio in self.scenerios:
+            self.assertEqual(
+                can_construct_memo(scenerio['target'], scenerio['words']),
+                scenerio['assert']
+            )
 
 
 class TestBestSum(unittest.TestCase):
@@ -25,16 +94,6 @@ class TestBestSum(unittest.TestCase):
         }
     ]
 
-    def test_best_sum_rec(self):
-        """
-        test for best sum without memoization
-        """
-        for scenerio in self.scenerios:
-            self.assertEqual(
-                best_sum_rec(scenerio['target'], scenerio['numbers']),
-                scenerio['assert']
-            )
-
     def test_best_sum_memo(self):
         """
         test for best sum WITH memoization
@@ -47,4 +106,7 @@ class TestBestSum(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    """
+    initiate tests from command line
+    """
     unittest.main()
